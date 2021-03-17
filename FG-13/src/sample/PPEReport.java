3f3f665740457a7f8a7fa14d5e342c031ea37ac9
AzizAlyoubi <alyobi.2020@gmail.com>
2021-03-17 08:47:13 +0300

    bug fix

--- FG-13/src/sample/PPEReport.java
+++ FG-13/src/sample/PPEReport.java
@@ -296,7 +296,7 @@ public class PPEReport extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        new inputMenu().setVisible(true);
+        new viewReport().setVisible(true);
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
